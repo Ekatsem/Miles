575d0eec71784f5bb77c58da9d83c0ae724ccc6b
Ekaterina <kat.sem@mail.ru>
2022-10-19 10:29:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 20_000;
+
+        int mile = 20;
+
+        int bonus = price / mile;
+
+        System.out.println("Бонусные мили - " + bonus);
+    }
+}
